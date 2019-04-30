181ec56342d49047e5f15227f71b9617f9d7fcec
Michael Schappacher <michael.schappacher@stelligent.com>
2019-04-30 16:05:25 -0400

    Changing layout and splitting cli builder object from programmatic.

--- Objects/cli.py
+++ Objects/cli.py
@@ -4,7 +4,7 @@ from Objects.layer import Layer
 from Objects.s3 import S3
 
 
-class LayerCreator:
+class LayerCreatorCli:
     def __init__(self, s3, layer, zip_name="layer.zip"):
         # TODO implement a CloudFormation template to register the layer and establish permissions for it
         """
@@ -21,26 +21,15 @@ class LayerCreator:
         Brings together the different cli methods into one simple call
         """
         if not self.s3.check_if_s3_exists():
-            if self.prompt_for_bucket_input():
-                self.create_s3()
-        if self.prompt_for_file_input():
-            if self.prompt_for_user_upload_input():
-                self.prompt_for_register_layer()
+            if self.__prompt_for_bucket_input():
+                self.__create_s3_cli()
+        if self.__prompt_for_file_input():
+            if self.__prompt_for_user_upload_input():
+                self.__prompt_for_register_layer()
 
-    @classmethod
-    def pre_zipped_layer_creator(cls, zip_path, bucket_name):
-        """
-        Allows you to create a layer by specifying an s3 bucket you have access to, and a path to the zip file.
-        :param zip_path: Path to the zipfile you want to upload
-        :param bucket_name: name of the s3 bucket you want to use
-        :return:
-        """
-        layer = Layer()
-        s3 = S3(bucket_name)
-        return cls(s3, layer, zip_path)
 
     @staticmethod
-    def _prompt_for_bucket_input():
+    def __prompt_for_bucket_input():
         """ Part 1 of the cli builder"""
         user_input = input("Bucket does not exist. Do you want to create this bucket? (Y, N): ")
         if user_input in ["Y", "y", "yes", "Yes"]:
@@ -52,7 +41,7 @@ class LayerCreator:
             print("Invalid Entry")
             exit()
 
-    def _prompt_for_file_input(self):
+    def __prompt_for_file_input(self):
         """Part 2 of the cli builder"""
         user_input = input("Is the layer you want to upload zipped? (Y, N): ")
         if user_input in ["Y", "y", "yes", "Yes"]:
@@ -64,7 +53,7 @@ class LayerCreator:
             if new_user_input in ["Y", "y", "yes", "Yes"]:
                 file_path = input("Please enter the filepath now: ")
                 try:
-                    self.prepare_layer(file_path)
+                    self.__prepare_layer_cli(file_path)
                     print("File Successfully Zipped")
                     return True
                 except FileNotFoundError:
@@ -77,27 +66,27 @@ class LayerCreator:
                 print("Invalid Entry")
         return False
 
-    def _prompt_for_user_upload_input(self):
+    def __prompt_for_user_upload_input(self):
         """Part 3 of the cli builder"""
         user_input = input("Upload layer?(Y, N): ")
         if user_input in ["Y", "y", "yes", "Yes"]:
-            self.upload_layer()
+            self.__upload_layer_cli()
             return True
 
         if user_input in ["N", "n", "No", "no"]:
             return False
 
-    def _prompt_for_register_layer(self):
+    def __prompt_for_register_layer(self):
         """Part 4 of the cli builder"""
         user_input = input("Do you want to register this layer?(Y, N): ")
         if user_input in ["Y", "y", "yes", "Yes"]:
-            self.define_layer_version()
+            self.__define_layer_version()
             return True
 
         if user_input in ["N", "n", "No", "no"]:
             return False
 
-    def _s3_response_constructor(self):
+    def __s3_response_constructor_cli(self):
         """
         Method to construct a response for the s3 create bucket.
         :return: dictionary of **kwargs
@@ -107,38 +96,55 @@ class LayerCreator:
         config["Bucket"] = self.s3.bucket_name
         return config
 
-    def create_s3(self):
+    def __create_s3_cli(self):
         """
         method to bring together the s3 object and config object to construct a response from the yaml file and pass
         to the create s3 bucket method.
 
-        :return: boto 3 response
+        :return: boto3 response
         """
-        pre_made_response = self._s3_response_constructor()
+        pre_made_response = self.__s3_response_constructor_cli()
         response = self.s3.create_bucket(pre_made_response)
         return response
 
     @staticmethod
-    def prepare_layer(file_path):
+    def __prepare_layer_cli(file_path):
         """
         Utility to zip files in preparation for upload
         :param file_path: location of file to zip
         """
         shutil.make_archive('layer', 'zip', file_path)
 
-    def upload_layer(self):
+    def __upload_layer_cli(self):
         """
         Passes in zip file name to the s3 function to upload to the specified s3 bucket
         """
         self.s3.upload_layer(self.file_path)
 
-    def _layer_response_creator(self):
+    def __layer_response_creator(self):
         config = ConfigReader.read_config("layer_config.yaml")
         config = config["Layer"]
         config["Content"]["S3Bucket"] = self.s3.bucket_name
         config["Content"]["S3Key"] = self.zip_name
         return config
 
-    def define_layer_version(self):
-        pre_made_response = self._layer_response_creator()
+    def __define_layer_version(self):
+        pre_made_response = self.__layer_response_creator()
         self.layer.publish_layer_version(pre_made_response)
+
+
+class LayerCreator:
+    def __init__(self):
+        pass
+
+    @classmethod
+    def pre_zipped_layer_creator(cls, zip_path, bucket_name):
+        """
+        Allows you to create a layer by specifying an s3 bucket you have access to, and a path to the zip file.
+        :param zip_path: Path to the zipfile you want to upload
+        :param bucket_name: name of the s3 bucket you want to use
+        :return:
+        """
+        layer = Layer()
+        s3 = S3(bucket_name)
+        return cls(s3, layer, zip_path)
